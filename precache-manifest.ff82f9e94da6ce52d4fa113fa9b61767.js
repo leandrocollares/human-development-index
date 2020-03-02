28e92c3564b9b215dd9ee112b50dacdc60cbaa7e
Leandro Collares <leco@uvic.ca>
2020-03-02 16:25:37 -0300

    Updates

---
+++ precache-manifest.ff82f9e94da6ce52d4fa113fa9b61767.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e11787f900f109e2050ef2c27668b040",
+    "url": "/human-development-index/index.html"
+  },
+  {
+    "revision": "93ba7304631b738b9ddf",
+    "url": "/human-development-index/static/css/main.07c1fe39.chunk.css"
+  },
+  {
+    "revision": "d096b0f0c802a3c7f5d2",
+    "url": "/human-development-index/static/js/2.9ee840b4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/human-development-index/static/js/2.9ee840b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93ba7304631b738b9ddf",
+    "url": "/human-development-index/static/js/main.ad31b77c.chunk.js"
+  },
+  {
+    "revision": "452214022503b23c6c51",
+    "url": "/human-development-index/static/js/runtime-main.6f028a5f.js"
+  },
+  {
+    "revision": "85e924d4bda98adf3b6a8f39de7ad6ea",
+    "url": "/human-development-index/static/media/hdi.85e924d4.csv"
+  }
+]);
